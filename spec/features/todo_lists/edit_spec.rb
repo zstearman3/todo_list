4930d912a70e9f1866ebbdb827446981b91c89f9
Zach Stearman <zstearman3@gmail.com>
2016-06-13 22:54:05 -0500

    Test editing todo list tools

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,22 +1,51 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
-  it "updates a todo list successfully with correct information" do
-    todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
+  let!(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list.")}
+  def update_todo_list(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is my todo list. "
+
+    todo_list = options[:todo_list]
 
     visit "/todo_lists"
     within "#todo_list_#{todo_list.id}" do
       click_link "Edit"
     end
 
-    fill_in "Title", with: "New Title"
-    fill_in "Description", with: "New description"
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Update Todo list"
+  end
+
+  it "updates a todo list successfully with correct information" do
+    update_todo_list todo_list: todo_list, title: "New Title", description: "New description."
 
     todo_list.reload()
-    
+
     expect(page).to have_content("Todo list was successfully updated")
     expect(todo_list.title).to eq("New Title")
-    expect(todo_list.description).to eq("New description")
+    expect(todo_list.description).to eq("New description.")
   end
+
+  it "displays an error with no title" do
+    update_todo_list todo_list: todo_list, title: ""
+    expect(page).to have_content("error")
+  end
+
+  it "displays an error with too short of a title" do
+    update_todo_list todo_list: todo_list, title: "hi"
+    expect(page).to have_content("error")
+  end
+
+  it "displays an error with no description" do
+    update_todo_list todo_list: todo_list, description: ""
+    expect(page).to have_content("error")
+  end
+
+  it "displays an error with too short of a description" do
+    update_todo_list todo_list: todo_list, description: "hi"
+    expect(page).to have_content("error")
+  end
+
 end
